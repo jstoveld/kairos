3d05500b0d29ff9fbdbb019f0be6401d5e90191c
John Stoveld <jstoveld@gmail.com>
2024-10-04 14:18:17 -0400

    creating dev branch for best practices, created readme file and main.py. Documented project requirements.

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+
+
+### REQIREMENTS ###
+
+## Authentication
+# 1. User needs to be able to create an account.
+# 2. User needs to be able to log in.
+# 3. Secure endponits with JWT.
+
+## Authentication Endpoints
+# 1. Register (POST) new user 
+# /register
+    # Response will be user object with JWT token.
+# 2. Login an existing user. 
+# /login
+    # Response will be user object with JWT token.
+
+
+
+
+## Image Management
+#1. User must be able to upload images.
+#2. Users must be able to perform  various operations on images.
+    # Resize
+    # Crop
+    # Rotate
+    # Watermark
+    # Flip
+    # Mirror
+    # Compress
+    # Change format (JPEG, PNG, etc.)
+    # Apply filters (grayscale, sepia, etc.)
+#3. Retrieve a saved image in different format.
+    ##a. PNG
+    ##b. JPEG
+    ##c. WEBP
+#4. List all uploaded images by a user with metadata.
+
+
