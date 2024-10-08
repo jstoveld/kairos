a93765ffebd4e11d8d301ceedc4fefde5d624e1f
John Stoveld <jstoveld@gmail.com>
2024-10-08 11:31:16 -0400

    datetime.utcnow is deprecated and has been replaced with datetime.timezone.utc. Revised missing quotes around key on image endpoint when unpacking Contents response.

--- main.py
+++ main.py
@@ -114,9 +114,9 @@ def authenticate_user(fake_db, email: str, password: str):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(datetime.timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -127,7 +127,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
-            status_code=400, detail="Incorrect username or password"
+            status_code=400, 
+            detail="Incorrect username or password",
             headers = {"WWW-Authenticate": "Bearer"}
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -199,11 +200,11 @@ async def rotate_image(degrees: int, file: UploadFile = File(...), current_user:
         return HTTPException(status_code=500, detail="Credentials not available")
     
 app.get("/images/")
-async def get_images(udrrent_user: dict = Depends(get_current_user)):
+async def get_images(current_user: dict = Depends(get_current_user)):
     try:
         response = s3.list_objects_v2(bucket=BUCKET_NAME)
         if 'Contents' in response:
-            return [{"filename": obj["key"], f"url": f"https://{BUCKET_NAME}.s3.amazonaws.com/{obj[key]}"} for obj in response['Contents']}]
+            return [{"filename": obj["key"], f"url": f"https://{BUCKET_NAME}.s3.amazonaws.com/{obj["key"]}"} for obj in response['Contents']]
         else:
             return []
     except NoCredentialsError:
